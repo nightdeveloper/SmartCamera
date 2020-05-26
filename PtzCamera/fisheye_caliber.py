6c79d15e818375dd80f6efec44057f96f03cddc4
Andrey Sergeew <avsergeev@neoflex.ru>
2020-05-27 00:14:19 +0300

    camera distortion calibration, second method

--- PtzCamera/fisheye_caliber.py
+++ PtzCamera/fisheye_caliber.py
@@ -12,25 +12,25 @@ logging.info("init calibration")
 # Chessboard dimensions
 CHESSBOARD_WIDTH = 8
 CHESSBOARD_HEIGHT = 6
+REF_IMAGE = 'TestImages/ref.jpg'
 
-object_point = np.zeros((CHESSBOARD_WIDTH*CHESSBOARD_HEIGHT, 3), np.float32)
-object_point[:, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
+object_point = np.zeros((1, CHESSBOARD_WIDTH*CHESSBOARD_HEIGHT, 3), np.float32)
+object_point[0, :, :2] = np.mgrid[0:CHESSBOARD_WIDTH, 0:CHESSBOARD_HEIGHT].T.reshape(-1, 2)
+
 
 # images with printed chessboard
 filenames = [
     "TestImages/1.jpg",
-    #"TestImages/2.jpg",
-    #"TestImages/3.jpg",
-    #"TestImages/4.jpg",
-    #"TestImages/5.jpg"
+    "TestImages/2.jpg",
+    "TestImages/3.jpg",
+    "TestImages/4.jpg",
+    "TestImages/5.jpg"
 ]
 
 image_size = []
 
 object_points = []
 image_points = []
-
-last_image_shape = []
 
 for filename in filenames:
 
@@ -39,7 +39,6 @@ for filename in filenames:
     logging.info("reading image...")
     image = cv2.imread(filename)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    last_image_shape = gray.shape[::-1]
 
     logging.info("finding chessboard corners...")
 
@@ -62,19 +61,60 @@ for filename in filenames:
     else:
         logging.info("can't find chessboard at the image " + filename)
 
+ref_image = cv2.imread(REF_IMAGE)
+(ref_image_shape_w, ref_image_shape_h) = ref_image.shape[:2]
+
+# first method
+
+logging.info(" --- first method")
+
 result, camera_matrix, distortion_coefficients, rotation_vectors, translation_vectors \
-    = cv2.calibrateCamera(object_points, image_points, last_image_shape, None, None)
+    = cv2.calibrateCamera(object_points, image_points, (ref_image_shape_w, ref_image_shape_h), None, None)
 
 logging.info("distortion coefficients = " + str(distortion_coefficients))
 
 logging.info("writing test image...")
-ref_image = cv2.imread('TestImages/ref.jpg')
-h,  w = ref_image.shape[:2]
-new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients, (w, h), 1, (w,h))
+new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients,
+                                                       (ref_image_shape_w, ref_image_shape_h), 1,
+                                                       (ref_image_shape_w, ref_image_shape_h))
 
 result_image = cv2.undistort(ref_image, camera_matrix, distortion_coefficients, None, new_camera_matrix)
 
-cv2.imwrite('TestImages/reg_out.jpg', result_image)
+cv2.imwrite('TestImages/out_first.jpg', result_image)
+
+
+# second method
+
+logging.info(" --- second method")
+
+calibration_flags = 0
+calibration_flags |= cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC
+calibration_flags |= cv2.fisheye.CALIB_FIX_SKEW
+
+K = np.zeros((3, 3))
+D = np.zeros((4, 1))
+
+r_vectors = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(len(filenames))]
+t_vectors = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(len(filenames))]
+
+cv2.fisheye.calibrate(
+    object_points,
+    image_points,
+    (ref_image_shape_w, ref_image_shape_h),
+    K,
+    D,
+    r_vectors,
+    t_vectors,
+    calibration_flags,
+    (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6))
+
+logging.info("distortion coefficients K = " + str(K) + ", D = " + str(D))
+
+logging.info("writing test image...")
+map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, ref_image.shape[:2], cv2.CV_16SC2)
+result_image = cv2.remap(ref_image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+cv2.imwrite('TestImages/out_second.jpg', result_image)
 
 logging.info("finished")
 
